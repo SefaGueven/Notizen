d15422e7c65cdc4529c03a03cd6280c038f970d9
Sefa Gueven <sefagueven@gmx.de>
2026-03-07 11:33:10 +0100

    stringify und parse befehle hinzugefügt

--- LocalStorage/script.js
+++ LocalStorage/script.js
@@ -1,34 +1,39 @@
 let myData=['Banana','not Banana','Apple'];
 
 function init() {
+
     getFromLocalStorage();
     render()
 }
 
 function savaData() {
-    let inputRef = document.getElementById('data_input');
+    let inputRef = document.getElementById('data_input'); //wird ausgelesen .
 
-        if(inputRef.value !=""){
-            myData.push(inputRef.value);
+        if(inputRef.value !=""){                            //Wenn es nicht leer ist wird es ausgelesen .
+            myData.push(inputRef.value);                    // wird in "myData geschrieben"
 
         }
     
-        saveToLocalStorage();
+        saveToLocalStorage();                           // wird hier gesichert.
 
         render();
         inputRef.value ="";
     
 }
 function saveToLocalStorage() {
+    localStorage.setItem("myData", JSON.stringify(myData));     // "stringify" wandelt ein Objekt oder Datenstruktur in einen Text im JSON-Format um.Stringify wandelt hier Array in String um.
+    
+}                                                                //setItem und getItem werden meistens beim Browser-Speicher verwendet.
+
+function getFromLocalStorage() {                                    
+    let myArr = JSON.parse(localStorage.getItem("myData"));       // "parse" wandelt den String wieder zurück ins Array.
+    myData = myArr;
     
 }
 
-function getFromLocalStorage() {
-    
-}
 function render() {
-    let contentRef =document.getElementById('content');
-    contentRef.innerHTML="";
+    let contentRef = document.getElementById('content');
+    contentRef.innerHTML = "";
 
     for (let index = 0; index < myData.length; index++) {
         contentRef.innerHTML += `<p>${myData[index]}</p>`
